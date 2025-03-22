e682f2b52bf515063299222c76626f7a2bb79476
mohammadali <80569235+BELECTRON13@users.noreply.github.com>
2025-03-22 15:17:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -23,22 +23,3 @@ document.querySelectorAll('.faq-question').forEach(question => {
         }, 1000);
     });
 });
-
-function openBaleLink(link) {
-            if (window.Bale && window.Bale.WebApp) {
-                window.Bale.WebApp.openLink(`https://ble.ir/${link}`, { try_instant_view: true });
-
-                setTimeout(function() {
-                    window.Bale.WebApp.close();
-                }, 1000);
-            }
-        }
-function openLink(link) {
-            if (window.Bale && window.Bale.WebApp) {
-                window.Bale.WebApp.openLink(`${link}`, { try_instant_view: true });
-
-                setTimeout(function() {
-                    window.Bale.WebApp.close();
-                }, 1000);
-            }
-        }
