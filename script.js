6f3cbff22e35f366dde3e652a6e2b80526e40f17
mohammadali <80569235+BELECTRON13@users.noreply.github.com>
2025-03-22 15:02:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -25,25 +25,20 @@ document.querySelectorAll('.faq-question').forEach(question => {
 });
 
 function openBaleLink(link) {
-    if (window.Bale && window.Bale.WebApp) {
-        window.Bale.WebApp.openLink(`https://ble.ir/${link}`, { try_instant_view: true });
+            if (window.Bale && window.Bale.WebApp) {
+                window.Bale.WebApp.openLink(`https://ble.ir/${link}`, { try_instant_view: true });
 
-        setTimeout(function() {
-            window.Bale.WebApp.close();
-        }, 1000);
-    } else {
-        window.location.href = `https://ble.ir/${link}`;
-    }
-}
-
+                setTimeout(function() {
+                    window.Bale.WebApp.close();
+                }, 1000);
+            }
+        }
 function openLink(link) {
-    if (window.Bale && window.Bale.WebApp) {
-        window.Bale.WebApp.openLink(`${link}`, { try_instant_view: true });
+            if (window.Bale && window.Bale.WebApp) {
+                window.Bale.WebApp.openLink(`${link}`, { try_instant_view: true });
 
-        setTimeout(function() {
-            window.Bale.WebApp.close();
-        }, 1000);
-    } else {
-        window.location.href = link;
-    }
-}
+                setTimeout(function() {
+                    window.Bale.WebApp.close();
+                }, 1000);
+            }
+        }
